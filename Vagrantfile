7714f0a16e9f360d2f8e5fb742d62ba3a502c7e7
bartek5186 <bartek5186@gmail.com>
2019-03-20 17:01:30 +0100

    [FIX] Handle args

--- Vagrantfile
+++ Vagrantfile
@@ -20,7 +20,6 @@ domains = {
   app: options['domain']
 }
 
-
 Vagrant.configure(2) do |config|
   config.vm.box = 'ubuntu/bionic64'
   config.vm.box_check_update = options['box_check_update']
@@ -44,8 +43,25 @@ Vagrant.configure(2) do |config|
   config.hostmanager.include_offline    = true
   config.hostmanager.aliases            = domains.values
 
-  config.vm.provision 'shell', path: './vagrant/provision/as-root.sh', args: [options['timezone']]
-  config.vm.provision 'shell', path: './vagrant/provision/as-vagrant.sh', args: [options['domain']], privileged: false
+  args = [
+    options['timezone'],
+    options['ip'],
+    options['accounts']['account_main'],
+    options['accounts']['account_empty_1'],
+    options['accounts']['account_empty_2'],
+    options['accounts']['account_sealer_1'],
+    options['accounts']['account_sealer_2'],
+    options['accounts']['account_alloc'],
+    options['domain'],
+    options['network_name'],
+    options['genesis_file'],
+    options['secret_api_password'],
+    options['location']
+  ]
+
+  config.vm.provision 'shell', path: './vagrant/provision/as-root.sh', args: args
+  config.vm.provision 'shell', path: './vagrant/provision/as-vagrant.sh', privileged: false
+  config.vm.provision 'shell', path: './vagrant/provision/configure-network.sh', args: args, privileged: false
   config.vm.provision 'shell', path: './vagrant/provision/always-as-root.sh', run: 'always'
 
 end
--- vagrant/provision/as-root.sh
+++ vagrant/provision/as-root.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 timezone=$(echo "$1")
+ip=$(echo "$2")
+account_main=$(echo "$3")
+account_empty_1=$(echo "$4")
+account_empty_2=$(echo "$5")
+account_sealer_1=$(echo "$6")
+account_sealer_2=$(echo "$7")
+account_alloc=$(echo "$8")
+domain=$(echo "$9")
 
 function info {
   echo " "
@@ -51,15 +59,15 @@ service sshd restart
 
 # Accoutnts Are pregenerated, define as env variable
 info "Create environment variables from wallets"
-echo 'ACC_MAIN="0xb2ba413fba6361bacc7a2055c2b28d6c93cf138e"' >> /etc/environment
-echo 'ACC_01="0xac5a29b299e6fc030abe9a23b16f05123136922c"' >> /etc/environment
-echo 'ACC_02="0xc17cdea27b2a4ff2ec0d52b2b61d41ebd55bad2b"' >> /etc/environment
-echo 'SEAL_01="0x59e3dfd51852cf0a0710b50982ce0de91576abf6"' >> /etc/environment
-echo 'SEAL_02="0x390c1c74d1d5ab47a876eab170ab98b5bb864165"' >> /etc/environment
-echo 'ACC_ALLOC="0x3e216b18e24c6cbe4c68e68b8c7bfebd664526c4"' >> /etc/environment
+echo 'ACC_MAIN="${account_main}"' >> /etc/environment
+echo 'ACC_01="${account_empty_1}"' >> /etc/environment
+echo 'ACC_02="${account_empty_2}"' >> /etc/environment
+echo 'SEAL_01="${account_sealer_1}"' >> /etc/environment
+echo 'SEAL_02="${account_sealer_2}"' >> /etc/environment
+echo 'ACC_ALLOC="${account_alloc}"' >> /etc/environment
 
 info "Create subdomains under hosts file"
-echo "192.168.66.66 faucet.eth.devel" >> /etc/hosts
-echo "192.168.66.66 wallet.eth.devel" >> /etc/hosts
-echo "192.168.66.66 stats.eth.devel" >> /etc/hosts
-echo "192.168.66.66 eth.devel" >> /etc/hosts
+echo "${ip} faucet.${domain}" >> /etc/hosts
+echo "${ip} wallet.${domain}" >> /etc/hosts
+echo "${ip} stats.${domain}" >> /etc/hosts
+echo "${ip} ${domain}" >> /etc/hosts
